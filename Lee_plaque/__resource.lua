e0e2a9cd6fe61031cd7474306a0eaa6d4dea83cd
Lee_dev <49000109+paro227@users.noreply.github.com>
2021-07-23 01:52:13 +0200

    Delete Lee_plaque directory

--- Lee_plaque/__resource.lua
+++
@@ -1,7 +0,0 @@
---BY BNJ-- 𝑯𝒐𝒎𝒎𝒆-𝒆𝒇𝒇𝒊𝒄𝒂𝒔𝒆
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
-client_script 'client.lua'
-client_script 'config.lua'
-server_script 'server.lua'
-server_script 'config.lua'

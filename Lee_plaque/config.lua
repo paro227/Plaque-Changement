b3872a86bff63239fbd556566b82c5d27ec20214
Lee_dev <49000109+paro227@users.noreply.github.com>
2021-07-23 01:52:33 +0200

    Add files via upload

---
+++ Lee_plaque/config.lua
@@ -0,0 +1,15 @@
+--BY LeePong--
+ConfigPlaqueBNJ = {
+	czas = 60, -- Temps après lequel la plaque peut être de nouveau vu (minutes)
+	cooldown = 60, -- Temps d'attente pour changer de nouveau la plaque
+	money = 10000, -- Prix de changement	
+	coords = { x = 210.061, y = -1690.394, z = 29.55 }, -- Coordonnées de l'atelier
+
+	
+	
+	-- Ped (https://wiki.rage.mp/index.php?title=Peds)
+	pedName = 'u_m_y_militarybum',
+	pedHash = 0x4705974A,
+	pedHeading = 71.5,
+	pedCoords = { x = 210.061, y = -1691.394, z = 29.55 },
+}
